d2cd089805396f8776a9fc11608d5531230eb7c8
luis120721 <freskimartinez@gmail.com>
2025-09-08 18:51:42 -0600

    Deploy portafolio a GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-}
+  basePath: '/portafolioluis.github.io', // <--- pon el nombre de tu repo
+  assetPrefix: '/portafolioluis.github.io/', // <--- para CSS, JS e imágenes
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
